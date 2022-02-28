89bb72d10a238d20afb5cbc7c0c7efccfe9d6973
yakattak <stevecalebrese@gmail.com>
2022-02-27 19:37:17 -0500

    employee passes test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,19 @@
 const Employee = require('../lib/Employee.js')
 
 test('creates employee object', () => {
-    const employee = new Employee('dave');
+    const employee = new Employee('dave', 444, 'hey@gmail.com', 'engineer');
     expect(employee.name).toBe('dave');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
+
+});
+
+test('gets employees name, id, email, role', () => {
+    const employee = new Employee('dave', 444, 'hey@gmail.com', 'engineer');
+
+    expect(employee.getName()).toBe('dave');
+    expect(employee.getId()).toEqual(444);
+    expect(employee.getEmail()).toBe('hey@gmail.com');
+    expect(employee.getRole()).toBe('Employee');
 
 });
