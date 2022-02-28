05c5d7e635600bd1a664020576962c5c67df3360
yakattak <stevecalebrese@gmail.com>
2022-02-28 16:10:16 -0500

    test fix

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,19 +2,19 @@ const Manager = require('../lib/Manager');
 
 test ('sets manager office number', () => {
 
-    const manager = new Manager ('dave', '444', 'hey@gmail.com', '72');
+    const manager = new Manager ('dave', '444', 'hey@gmail.com', "Manager", '72');
     expect (manager.officeNumber).toBe('72');
 
 })
 
 test('get manager role', () => {
-    const manager = new Manager ('dave', '444', 'hey@gmail.com', '72');
+    const manager = new Manager ('dave', '444', 'hey@gmail.com', "Manager", '72');
     expect(manager.getRole()).toBe('Manager');
 });
 
 
 
 test('get office number', () => {
-    const manager = new Manager ('dave', '444', 'hey@gmail.com', '72');
+    const manager = new Manager ('dave', '444', 'hey@gmail.com', "Manager", '72');
     expect(manager.getOfficeNumber()).toBe('72');
 });
